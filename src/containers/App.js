72e07e2482bd6b1ae59e322d1d2d5e425ad78dfa
Jalal-Eddine El-Habbazi <Jalaleddine.elhabbazi@gmail.com>
2020-02-21 00:32:29 +0100

    ffff

---
+++ src/containers/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
+
+import {setSearchField } from '../actions'
+
+const mapStateToProps = state => {
+  return {
+    searchfield: state.searchRobots.searchfield
+  }
+}
+
+const mapDispatchProps = (dispatch) => {
+  return {onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
+
+class App extends Component {
+  constructor() { //help define a state
+    super()    //call the constructor of the Component 
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});   
+  }
+  
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+            </ErrorBoundry>
+          </Scroll>
+        </div>
+      );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchProps)(App);
+//connect is a higher order function --> return another function
